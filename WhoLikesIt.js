8e9f6773bf621ec41d6fa5e1940546d9fadfd61b
Joshua Peavler <joshuaapeavler@gmail.com>
2020-06-16 16:41:39 -0600

    Added completed Who Likes It Challenge from Codewars

---
+++ WhoLikesIt.js
@@ -0,0 +1,15 @@
+function likes(names) {
+    let result;
+    if(names.length == 0) {
+      result = "no one likes this";
+    } else if(names.length == 1) {
+      result = `${names[0]} likes this`;
+    } else if(names.length == 2) {
+      result = `${names[0]} and ${names[1]} like this`;
+    } else if(names.length < 4) {
+      result = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    } else if(names.length >= 4) {
+      result = `${names[0]}, ${names[1]} and ${names.length -2} others like this`;
+    }
+    return result;
+  }
